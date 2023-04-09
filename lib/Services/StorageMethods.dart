35bd47c1ebf68cd85924384833caa9a878aab049
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2023-04-09 11:04:25 +0530

    some

--- lib/Services/StorageMethods.dart
+++ lib/Services/StorageMethods.dart
@@ -1,17 +1,16 @@
 import 'dart:typed_data';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:uuid/uuid.dart';
 
 class StorageMethods {
   final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
-
+  final prefs = GetStorage();
   Future<String> uploadImageToFirebase(
       String childname, Uint8List file, bool isPost) async {
-    Reference ref = _firebaseStorage
-        .ref()
-        .child(childname)
-        .child("5mOCKrOcDuf3iqBp64Ls38boZ973");
+    Reference ref =
+        _firebaseStorage.ref().child(childname).child(prefs.read("user_id"));
     if (isPost == true) {
       var postID = const Uuid().v1();
       ref = ref.child(postID);
